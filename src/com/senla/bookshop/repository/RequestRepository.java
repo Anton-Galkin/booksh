7f6eafcab8157d7df55771f1597d14d9a3ed3cc9
Vladimir <v.m.bolshov@gmail.com>
2022-08-28 10:07:28 +0300

    RequestRepositoty refactoring

--- src/com/senla/bookshop/repository/RequestRepository.java
+++ src/com/senla/bookshop/repository/RequestRepository.java
@@ -1,63 +1,57 @@
 package com.senla.bookshop.repository;
 
-import com.senla.bookshop.model.Book;
-import com.senla.bookshop.model.Request;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.senla.bookshop.model.Request;
+
 public class RequestRepository {
-    private ArrayList<Request> requests = new ArrayList<>();
+	private ArrayList<Request> requests = new ArrayList<>();
 
-    public int add(Request request) {
-        request.setId(getNextId());
-        requests.add(request);
-        return request.getId();
-    }
+	public int add(Request request) {
+		request.setId(getNextId());
+		requests.add(request);
+		return request.getId();
+	}
 
-    private int getNextId() {
-        return requests.size();
-    }
+	public boolean contains(Request request) {
+		return requests.contains(request);
+	}
 
-    public Optional<Request> get(int id) {
-        for (Request request : requests) {
-            if (request.getId() == id) {
-                return Optional.of(request);
-            }
-        }
+	public Optional<Request> get(int id) {
+		for (Request request : requests) {
+			if (request.getId() == id) {
+				return Optional.of(request);
+			}
+		}
 
-        return Optional.empty();
-    }
+		return Optional.empty();
+	}
 
+	public List<Request> getAll() {
+		return requests;
+	}
 
-    public void remove(Request request) {
-        requests.remove(request);
-    }
+	public void remove(int id) {
+		for (Request request : requests) {
+			if (request.getId() == id) {
+				requests.remove(request);
+				return; // TODO why return? 
+			}
+		}
+	}
 
-    public void remove(int id) {
-        for (Request request : requests) {
-            if (request.getId() == id) {
-                requests.remove(request);
-                return;  //TODO why return?
-            }
-        }
-    }
+	public void remove(Request request) {
+		requests.remove(request);
+	}
 
-    public void update(Request request) {
-        remove(request.getId());
-        requests.add(request);
-    }
+	public void update(Request request) {
+		remove(request.getId());
+		requests.add(request);
+	}
 
-    public int getNexId() {
-        return requests.size();
-    }
-
-    public List<Request> getAll() {
-        return requests;
-    }
-
-    public boolean contains(Request request) {
-        return requests.contains(request);
-    }
+	private int getNextId() {
+		return requests.size();
+	}
 }
