cedd8cae7a1ce9b02b054e3a3ee29d13652d3927
Vladimir <v.m.bolshov@gmail.com>
2022-09-07 22:54:48 +0300

    Start to implement sorting

--- src/com/senla/bookshop/repository/RequestRepository.java
+++ src/com/senla/bookshop/repository/RequestRepository.java
@@ -1,14 +1,20 @@
 package com.senla.bookshop.repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
 
-import com.senla.bookshop.model.Order;
+import com.senla.bookshop.model.Book;
 import com.senla.bookshop.model.Request;
 
 public class RequestRepository {
+
 	private ArrayList<Request> requests = new ArrayList<>();
 
 	public int add(Request request) {
@@ -35,6 +41,55 @@ public class RequestRepository {
 		return requests;
 	}
 
+	public List<Request> getAllSortByAlphabet() {
+		List<Request> list = new ArrayList<>();
+		list.addAll(requests);
+
+		list.sort(new Comparator<Request>() {
+
+			@Override
+			public int compare(Request request1, Request request2) {
+				return request1.getBook().getTitle().compareTo(request2.getBook().getTitle());
+			}
+		});
+		return list;
+	}
+
+	public List<Request> getAllSortByQuantity() { // TODO add field QUANTITY
+		List<Request> list = new ArrayList<>();
+		list.addAll(requests);
+
+//		list.sort(new Comparator<Request>() {
+//
+//			@Override
+//			public int compare(Request request1, Request request2) {
+//				return request1.getBook().getTitle().compareTo(request2.getBook().getTitle());
+//			}
+//		});
+		
+		HashMap<Book, Integer> requestMap = new HashMap<Book, Integer>();
+		
+		for (Request request : requests) {
+//			if (!requestMap.containsKey(request.getBook())) {
+//				requestMap.put(request.getBook(), 1);
+//			} else {
+//				requestMap.
+//			}
+			
+			Integer i = requestMap.putIfAbsent(request.getBook(), 1);
+			
+			if (i != null) {
+				requestMap.put(request.getBook(), ++i);
+			}
+			
+			Set<Entry<Book, Integer>> entrySet = requestMap.entrySet();
+			
+			
+		}
+		
+		return list;
+	}
+
 	public void remove(int id) {
 		for (Request request : requests) {
 			if (request.getId() == id) {
@@ -55,33 +110,5 @@ public class RequestRepository {
 
 	private int getNextId() {
 		return requests.size();
-	}
-
-	public List<Request> getAllSortByAlphabet() {
-		List<Request> list = new ArrayList<>();
-		list.addAll(requests);
-
-		list.sort(new Comparator<Request>() {
-
-			@Override
-			public int compare(Request request1, Request request2) {
-				return request1.getBook().getTitle().compareTo(request2.getBook().getTitle());
-			}
-		});
-		return list;
-	}
-
-	public List<Request> getAllSortByQuantity() { //TODO add field QUANTITY
-		List<Request> list = new ArrayList<>();
-		list.addAll(requests);
-
-		list.sort(new Comparator<Request>() {
-
-			@Override
-			public int compare(Request request1, Request request2) {
-				return request1.getBook().getTitle().compareTo(request2.getBook().getTitle());
-			}
-		});
-		return list;
 	}
 }
